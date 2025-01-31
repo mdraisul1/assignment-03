433f26583254ced4d8cbafce5b0dac7ea00501fa
mtmsujan <mdraju3248@gmail.com>
2025-01-31 15:55:58 +0600

    Added header logo section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+    extend: {
+      fontFamily: {
+        adlam : ['ADLaM Display', 'serif'],
+      },
+      colors: {
+        primary: "#FFF9F4",
+        secondary: "#F9DF8A",
+        link_bg: "#FFDC26",
+        accent: "#FFDC26",
+      },
+    },
   },
   plugins: [],
 }
